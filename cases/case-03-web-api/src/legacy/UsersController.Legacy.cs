22d579db867021e6d32b7946d7f9e821c5b23454
viniespuri <58437762+viniespuri@users.noreply.github.com>
2026-02-23 17:00:27 -0300

    Update UsersController.Legacy.cs

--- cases/case-03-web-api/src/legacy/UsersController.Legacy.cs
+++ cases/case-03-web-api/src/legacy/UsersController.Legacy.cs
@@ -2,31 +2,14 @@ using System.Web.Http;
 
 namespace Case3.Legacy
 {
-    [RoutePrefix("v1/users")]
     public class UsersController : ApiController
     {
-        /// <summary>
-        /// Corrige o endpoint legado para o contrato GET /v1/users/{id}, validando id invalido (400),
-        /// retornando 404 quando o usuario nao for encontrado e 200 no sucesso.
-        /// </summary>
+        [Route("v1/users/")]
         [HttpGet]
-        [Route("{id:int}")]
         public IHttpActionResult GetUser([FromUri] int id)
         {
-            if (id <= 0)
-            {
-                return BadRequest("id must be greater than zero.");
-            }
-
-            var userService = new UserService();
-            var user = userService.GetUser(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(user);
+            UserService USER_Service = new UserService();
+            return USER_Service.GetUser(id);
         }
     }
 }
